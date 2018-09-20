abb9fad3859bf8c86a7dff864a2b0b97446377dd
Jyguy <jskekkx@gmail.com>
2018-09-20 12:36:36 -0400

    Added ?avatar

---
+++ commands/misc/avatar.js
@@ -0,0 +1,21 @@
+module.exports = async (Client, message, args) => {
+  if (!args[1]) var member = message.member;
+  // eslint-disable-next-line no-redeclare
+  else var member = message.guild.members.get(args[1].replace(/[<>@!?]/g, ''));
+  if (!member) return message.reply('That member does not exist!');
+
+  const embed = new Client.Discord.MessageEmbed()
+    .setTitle(`${member.user.tag}'s Avatar`)
+    .setImage(member.user.displayAvatarURL({ size: 2048 }))
+    .setColor(member.displayHexColor);
+
+  return message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: 'avatar',
+  desc: 'Displays your or someone else\'s profile picture.',
+  category: 'misc',
+  usage: '?avatar [Member]',
+  aliases: ['av', 'pfp', 'profilepic']
+};
