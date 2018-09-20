2d034434b31c8fc4cb7bb2435d748b19535e8ed6
Jyguy <jskekkx@gmail.com>
2018-09-20 12:30:37 -0400

    Added ?afk

---
+++ commands/misc/afk.js
@@ -0,0 +1,34 @@
+const isAFK = new Set();
+
+module.exports = async (Client, message, args) => {
+  if (isAFK.has(message.author.id + message.guild.id)) return message.reply('You are already AFK!');
+
+  // If no reason was provided
+  if (!args[1]) var reason = 'None';
+  // eslint-disable-next-line no-redeclare
+  else var reason = args.slice(1).join(' ');
+
+  isAFK.add(message.author.id + message.guild.id);
+  message.channel.send(`You are now AFK for the reason of \`${Client.escapeMarkdown(reason)}\``);
+
+  const filter = m => m.mentions.users.has(message.author.id) || m.author.id === message.author.id;
+  const collector = message.channel.createMessageCollector(filter);
+
+  collector.on('collect', msg => {
+    if (msg.author.id === message.author.id) {
+      collector.stop();
+      isAFK.delete(message.author.id + message.guild.id);
+      return message.channel.send(`${message.author.tag}, Welcome back!`);
+    } else {
+      return message.reply(`${message.member} is currently AFK for \`${Client.escapeMarkdown(reason)}\`.`);
+    }
+  });
+};
+
+module.exports.help = {
+  name: 'afk',
+  desc: 'Displays that you are AFK.',
+  category: 'misc',
+  usage: '?afk [Reason]',
+  aliases: []
+};
