82401f9b62ba3bc66a081c82633bee7713882376
Jyguy <jskekkx@gmail.com>
2018-09-25 16:21:50 -0400

    Update ?eval

--- commands/misc/eval.js
+++ commands/misc/eval.js
@@ -15,13 +15,13 @@ module.exports = async (Client, message, args) => {
 
   try {
     // eslint-disable-next-line no-eval
-    let evaled = eval(code);
+    let evaled = await eval(code);
     if (typeof (evaled) !== 'string') evaled = require('util').inspect(evaled);
 
     let embed = new Client.Discord.MessageEmbed()
       .setAuthor('Evaluation')
       .setTitle('Output')
-      .setDescription(`\`\`\`xl\n${clean(evaled).length <= 1024 ? clean(evaled) : 'Over 1024 Characters'}\n\`\`\``)
+      .setDescription(`\`\`\`xl\n${clean(evaled).length <= 2048 ? clean(evaled) : 'Over 2048 Characters'}\n\`\`\``)
       .setColor(0x00FFFF)
       .setTimestamp();
     message.channel.send(embed);
