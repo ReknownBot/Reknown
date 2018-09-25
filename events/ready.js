743ad6bd395fb044ccd51ccd11161639b06026ef
Jyguy <jskekkx@gmail.com>
2018-09-25 16:21:25 -0400

    Update the status message to something... :smirk:

--- events/ready.js
+++ events/ready.js
@@ -2,18 +2,10 @@ module.exports = (Client, bot) => {
   // Logs something
   console.log(`Logged in as ${bot.user.tag}!`);
 
-  bot.user.setActivity(`${bot.guilds.size} Servers`, {
+  bot.user.setActivity('you in your sleep 👀', {
     // Sets the activity type for the bot as "Watching"
     type: 'WATCHING'
   });
-
-  bot.setInterval(() => {
-    // Sets the activity for the bot.
-    bot.user.setActivity(`${bot.guilds.size} Servers`, {
-      // Sets the activity type for the bot as "Watching"
-      type: 'WATCHING'
-    });
-  }, 900000);
 
   // Connects to the postgres database.
   return Client.sql.connect();
