3ebe119c5a58558cf4b04f06b479ac60edd82486
Jyguy <jskekkx@gmail.com>
2018-09-27 16:24:52 -0400

    Added a check of deleteinvite is enabled or not

--- functions/deleteinvite.js
+++ functions/deleteinvite.js
@@ -1,4 +1,7 @@
 module.exports = async (Client, message) => {
+  const enabled = (await Client.sql.query('SELECT bool FROM deleteinvite WHERE guildid = $1 LIMIT 1', [message.guild.id])).rows[0];
+  if (!enabled || !enabled.bool) return;
+
   // If the message does not contain an invite
   if (!/(?:https?:\/\/)?discord(?:app.com\/invite|.gg)\/[\w\d]+/gi.test(message.content)) return;
 
