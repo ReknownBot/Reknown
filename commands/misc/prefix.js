d866d0884844510908a1bfaa01e0f513787cb07a
Jyguy <jskekkx@gmail.com>
2018-10-02 15:50:32 -0400

    Added ?prefix

---
+++ commands/misc/prefix.js
@@ -0,0 +1,13 @@
+module.exports = async (Client, message, args) => {
+  const prefixRow = (await Client.sql.query('SELECT customprefix FROM prefix WHERE guildid = $1 LIMIT 1', [message.guild.id])).rows[0];
+  const prefix = prefixRow ? prefixRow.customprefix : '?';
+  return message.channel.send(`The current prefix on this server is \`${Client.escapeMarkdown(prefix)}\`. Looking to change the prefix? Take a look at \`${prefix}config prefix <New Prefix>\`.`);
+};
+
+module.exports.help = {
+  name: 'prefix',
+  desc: 'Displays the current prefix for the server.',
+  category: 'misc',
+  usage: '?prefix',
+  aliases: []
+};
