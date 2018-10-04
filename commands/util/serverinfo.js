2b4b0445161aceac8d5fadab0bc5b38a8dd9ec9d
Jyguy <jskekkx@gmail.com>
2018-10-04 12:03:31 -0400

    Added ?serverinfo

---
+++ commands/util/serverinfo.js
@@ -0,0 +1,24 @@
+module.exports = async (Client, message, args) => {
+  const embed = new Client.Discord.MessageEmbed()
+    .setTitle(`${message.guild.name} Server Info`)
+    .setColor(0x00FFFF)
+    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
+    .setThumbnail(message.guild.iconURL)
+    .addField('Member Count', message.guild.memberCount, true)
+    .addField('Owner', message.guild.owner.user.tag, true)
+    .addField('Server Created', Client.dateFormat(message.guild.createdAt, 'mmmm dS, yyyy, h:MM:ss TT'), true)
+    .addField('Role Count', message.guild.roles.size, true)
+    .addField('Channel Count', message.guild.channels.size, true)
+    .addField('Standard Emoji Count', message.guild.emojis.filter(e => !e.animated).size, true)
+    .addField('Animated Emoji Count', message.guild.emojis.filter(e => e.animated).size, true);
+
+  return message.channel.send(embed);
+};
+
+module.exports.help = {
+  name: 'serverinfo',
+  desc: 'Displays information about the server.',
+  category: 'util',
+  usage: '?serverinfo',
+  aliases: ['sinfo', 'guildinfo', 'ginfo']
+};
