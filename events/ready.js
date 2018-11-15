6983cbce1e39c85c79c2febe954f2bfb9f6c7f93
Jyguy <jskekkx@gmail.com>
2018-11-14 19:45:00 -0500

    Filters bots in activity

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = (Client, bot) => {
   // Logs something
   console.log(`Logged in as ${bot.user.tag}!`);
 
-  bot.user.setActivity(`${bot.users.size} Users and ${bot.guilds.size} Servers`, {
+  bot.user.setActivity(`${bot.users.filter(u => !u.bot).size} Users and ${bot.guilds.size} Servers`, {
     // Sets the activity type for the bot as "Watching"
     type: 'WATCHING'
   });
