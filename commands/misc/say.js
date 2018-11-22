0d8ddba5ba6616a2910836255ec8da8d714b6e21
Jyguy <jskekkx@gmail.com>
2018-11-22 11:19:10 -0500

    fix: Added a permission check on ?say

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -1,4 +1,6 @@
 module.exports = async (Client, message, args) => {
+  if (!Client.checkClientPerms(message.channel, 'EMBED_LINKS')) return message.reply('I do not have the required permission `Embed Links`!');
+
   if (!args[1]) return message.reply('You have to include a message for me to say!');
 
   const embed = new Client.Discord.MessageEmbed()
