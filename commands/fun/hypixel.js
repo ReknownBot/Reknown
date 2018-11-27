fb4e8e39f4bb07fc7e70126a019c1fb84ce8e200
Jyguy <jskekkx@gmail.com>
2018-11-26 20:55:55 -0500

    fix: hypixel.js - Removed await-to-js - Added a check if player.voting exists (It's undefined when the player has not voted.)

--- commands/fun/hypixel.js
+++ commands/fun/hypixel.js
@@ -1,5 +1,3 @@
-const { to } = require('await-to-js');
-
 module.exports = async (Client, message, args) => {
   if (!args[1]) return message.reply('You have to provide what I should search!');
   const options = ['player', 'guild', 'guildmember'];
@@ -10,28 +8,29 @@ module.exports = async (Client, message, args) => {
   const hypixel = Client.hypixel;
 
   if (option === 'player') {
-    const [err, { player }] = await to(hypixel.getPlayer('name', args[2]));
-    if (err) return message.reply('That player name is not valid!');
+    const info = await hypixel.getPlayer('name', args[2]).catch(() => 'failed');
+    if (info === 'failed') return message.reply('That player name is not valid!');
+    const player = info.player;
 
     const embed = new Client.Discord.MessageEmbed()
-      .setTitle(`${player.displayname}'s Hypixel Stats`)
+      .setTitle(`${Client.escapeMarkdown(player.displayname)}'s Hypixel Stats`)
       .setURL(`https://hypixel.net/player/${player.displayname}/`)
       .addField('Achievements Completed', Object.values(player.achievements).reduce((a, b) => a + b, 0), true)
       .addField('Network Exp', player.networkExp, true)
       .addField('Karma', player.karma, true)
-      .addField('Votes', player.voting.total, true)
+      .addField('Votes', player.voting ? player.voting.total : '0', true)
       .setColor(0x00FFFF);
 
     return message.channel.send(embed);
   } else if (option === 'guild') {
-    const [err, { guild: guildid }] = await to(hypixel.findGuild('name', args.slice(2).join(' ')));
-    if (err) return message.reply('I did not find a guild by that name!');
+    const info = await hypixel.findGuild('name', args.slice(2).join(' '));
+    if (!info.guild) return message.reply('I did not find a guild by that name!');
+    const guildid = info.guild;
 
-    // eslint-disable-next-line no-unused-vars
-    const [error, { guild }] = await to(hypixel.getGuild(guildid));
+    const { guild } = await hypixel.getGuild(guildid);
 
     const embed = new Client.Discord.MessageEmbed()
-      .setTitle(`${guild.name} Guild Stats`)
+      .setTitle(`${Client.escapeMarkdown(guild.name)} Guild Stats`)
       .setURL(`https://hypixel.net/guilds/${guild.name.replace(' ', '%20')}/`)
       .addField('Member Count', guild.members.length)
       .addField('Guild Tag', guild.tag ? guild.tag : 'None')
@@ -40,14 +39,13 @@ module.exports = async (Client, message, args) => {
 
     return message.channel.send(embed);
   } else if (option === 'guildmember') {
-    const [err, guild] = await to(hypixel.findGuild('memberName', args[2]));
-    if (err || !guild.guild) return message.reply('I did not find a guild by that member\'s name!');
+    const info = await hypixel.findGuild('memberName', args[2]).catch(() => 'failed');
+    if (info === 'failed' || !info.guild) return message.reply('I did not find a guild by that member\'s name!');
 
-    // eslint-disable-next-line no-unused-vars
-    const [error, { guild: guildInfo }] = await to(hypixel.getGuild(guild.guild));
+    const { guild: guildInfo } = await hypixel.getGuild(info.guild);
 
     const embed = new Client.Discord.MessageEmbed()
-      .setTitle(`${guildInfo.name} Guild Stats`)
+      .setTitle(`${Client.escapeMarkdown(guildInfo.name)} Guild Stats`)
       .setURL(`https://hypixel.net/guilds/${guildInfo.name.replace(' ', '%20')}/`)
       .addField('Member Count', guildInfo.members.length)
       .addField('Guild Tag', guildInfo.tag ? guildInfo.tag : 'None')
