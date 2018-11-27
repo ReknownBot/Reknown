ed2b376f777313bc0ea8bc7743945100ba7a0db1
Jyguy <jskekkx@gmail.com>
2018-11-26 20:57:05 -0500

    fix: id.js - Fixed incorrect help message - Added a possibility to have a role name check

--- commands/util/id.js
+++ commands/util/id.js
@@ -3,7 +3,7 @@ module.exports = async (Client, message, args) => {
   if (!args[1]) x = message.member;
   else {
     const possibleMember = message.guild.members.get(args[1].replace(/[<>@!?]/g, ''));
-    const possibleRole = message.guild.roles.get(args[1].replace(/[<>@&]/g, ''));
+    const possibleRole = message.guild.roles.get(args[1].replace(/[<>@&]/g, '')) || message.guild.roles.find(r => r.name.toLowerCase() === args.slice(1).join(' '));
     const possibleChannel = message.guild.channels.get(args[1].replace(/[<>#]/g, ''));
     if (possibleMember) x = possibleMember;
     else if (possibleRole) x = possibleRole;
@@ -16,8 +16,8 @@ module.exports = async (Client, message, args) => {
 
 module.exports.help = {
   name: 'id',
-  desc: 'Displays the ID of a member.',
+  desc: 'Displays the ID of something.',
   category: 'util',
-  usage: '?id [Member]',
-  aliases: ['memberid']
+  usage: '?id [Something]',
+  aliases: []
 };
