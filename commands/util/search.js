aec63550d975383d8de387d614dc7350a0b284a5
Jyguy <jskekkx@gmail.com>
2019-05-06 21:28:21 -0400

    Changed the max limit for searching 500 -> 100 due to rate limits.

--- commands/util/search.js
+++ commands/util/search.js
@@ -9,54 +9,34 @@ module.exports = async (Client, message, args) => {
   if (!member) return Client.functions.get('argFix')(Client, message.channel, 1, 'Did not find a member with that query.');
 
   if (!args[2]) return Client.functions.get('argMissing')(message.channel, 2, 'an amount of messages to search for');
-  let amt = args[2];
+  const amt = args[2];
   if (isNaN(amt)) return Client.functions.get('argFix')(Client, message.channel, 2, 'The amount must be a number.');
-  if (amt > 500) return Client.functions.get('argFix')(Client, message.channel, 2, 'The amount may not exceed 500.');
+  if (amt > 100) return Client.functions.get('argFix')(Client, message.channel, 2, 'The amount may not exceed 100.');
   if (amt < 1) return Client.functions.get('argFix')(Client, message.channel, 2, 'The amount may not be lower than 1.');
   if (amt.includes('.')) return Client.functions.get('argFix')(Client, message.channel, 2, 'The amount may not include a decimal.');
 
-  const max = Math.ceil(amt / 100);
-  const arr = [];
-  let num = 0;
-  let lastID;
-  for (let i = 0; i < max; i++) {
-    let msgs;
-    if (amt > 100) {
-      msgs = await message.channel.messages.fetch({
-        limit: 100,
-        before: lastID
-      });
-    } else {
-      msgs = await message.channel.messages.fetch({
-        limit: amt,
-        before: lastID
-      });
-    }
-
-    const messages = msgs.filter(m => m.author.id === member.id).sort((a, b) => {
-      // Message Created after
-      if (a.createdTimestamp > b.createdTimestamp) {
-        return -1;
-      } else if (a.createdTimestamp < b.createdTimestamp) {
-        return 1;
-      }
-      // Very slim chance of happening
-      return 0;
-    });
-    lastID = messages.last() ? messages.last().id : null;
-    messages.forEach(m => {
-      if (m.author.id === member.id) {
-        num++;
-        arr.push(`\`${num}. ${Client.escMD(m.content)}\``);
-      }
-    });
-    amt -= 100;
-  }
-
-  if (arr.length === 0) return message.reply('I did not find anything!');
-  return message.channel.send(arr, {
-    split: true
+  const msgs = await message.channel.messages.fetch({
+    limit: amt
   });
+
+  const sorted = msgs.filter(m => m.author.id === member.id).sort((a, b) => {
+    if (a.createdTimestamp > b.createdTimestamp) {
+      return -1;
+    } else if (a.createdTimestamp < b.createdTimestamp) {
+      return 1;
+    }
+    return 0;
+  });
+
+  if (sorted.size === 0) return message.reply('I did not find anything!');
+
+  let num = 0;
+  const msg = sorted.map(m => {
+    num += 1;
+    return `\`${num}. ${Client.escMD(m.content)}\``;
+  });
+  if (msg.length > 2048) return message.reply('The result is too large for me to send. Try toning down the limit for searching.');
+  return message.channel.send(msg);
 };
 
 module.exports.help = {
