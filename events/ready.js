efbd0e851f40f6a6c1b9fad7991125fbcd5f263b
Jyguy <jskekkx@gmail.com>
2019-05-23 10:34:47 -0400

    Simplified some code.

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,15 @@
+function unmute(Client, row) {
+  Client.sql.query('DELETE FROM mute WHERE userid = $1 AND guildid = $2', [row.userid, row.guildid]);
+
+  const guild = Client.bot.guilds.get(row.guildid);
+  if (!guild || !guild.me.hasPermission('MANAGE_ROLES')) return;
+  const member = guild.members.get(row.userid);
+  if (!member) return;
+  const role = member.roles.find(r => r.name === 'Muted');
+  if (!role || role.position >= guild.me.roles.highest.position) return;
+  return member.roles.remove(role);
+}
+
 /**
  * @param {import('../structures/client.js')} Client
  */
@@ -11,28 +23,11 @@ module.exports = Client => {
 
     const { rows: muteRows } = await Client.sql.query('SELECT * FROM mute');
     muteRows.forEach(row => {
-      if (Date.now() - row.mutedat > row.time) {
-        Client.sql.query('DELETE FROM mute WHERE userid = $1 AND guildid = $2', [row.userid, row.guildid]);
-        const guild = Client.bot.guilds.get(row.guildid);
-        if (!guild || !guild.me.hasPermission('MANAGE_ROLES')) return;
-        const member = guild.members.get(row.userid);
-        if (!member) return;
-        const role = member.roles.find(r => r.name === 'Muted');
-        if (!role || role.position >= guild.me.roles.highest.position) return;
-        return member.roles.remove(role);
-      }
+      if (Date.now() - row.mutedat > row.time) unmute(Client, row);
       const timeout = setTimeout(() => {
         clearTimeout(Client.mutes.get(row.userid));
         Client.mutes.delete(row.userid);
-        Client.sql.query('DELETE FROM mute WHERE userid = $1 AND guildid = $2', [row.userid, row.guildid]);
-
-        const guild = Client.bot.guilds.get(row.guildid);
-        if (!guild || !guild.me.hasPermission('MANAGE_ROLES')) return;
-        const member = guild.members.get(row.userid);
-        if (!member) return;
-        const role = member.roles.find(r => r.name === 'Muted');
-        if (!role || role.position >= guild.me.roles.highest.position) return;
-        return member.roles.remove(role);
+        unmute(Client, row);
       }, row.time);
       Client.mutes.set(row.userid, timeout);
     });
