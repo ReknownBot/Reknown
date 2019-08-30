5a929874b62801746f900d04a3e21f1f3f3ac987
Jyguy <jskekkx@gmail.com>
2019-08-30 00:04:47 -0400

    Fix incorrect argument index for ?ban when searching for user

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
     return message.channel.send(`Successfully banned member ${client.escMD(member.user.tag)} (ID: ${member.id})${reason ? ` for reason \`${client.escMD(reason)}\`` : ''}.`);
   }
 
-  const user = await client.functions.parseMention(args[0], message.guild, { type: 'user', client: client }).catch(() => false);
+  const user = await client.functions.parseMention(args[1], message.guild, { type: 'user', client: client }).catch(() => false);
   if (!user) return client.functions.badArg(message, 1, `I did not find a user by that query (${client.escMD(args[0])}).`);
   message.guild.members.ban(user, { reason: reason });
   return message.channel.send(`Successfully banned user ${client.escMD(user.tag)} (ID: ${user.id})${reason ? ` for reason \`${client.escMD(reason)}\`` : ''}.`);
