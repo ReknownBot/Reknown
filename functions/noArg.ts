34074e24c6f7ad0a49033ec16412e6ecd9993a1a
Jyguy <jskekkx@gmail.com>
2019-09-18 16:07:48 -0400

    allow certain functions to be ran with DMs

--- functions/noArg.ts
+++ functions/noArg.ts
@@ -3,7 +3,7 @@ import { embedColor } from '../config.json';
 
 
 module.exports = (message: Message, argNum: number, desc: string): void => {
-  if (!(message.channel as TextChannel).permissionsFor(message.guild.me).has('EMBED_LINKS')) return void message.channel.send(`:x: Argument **#${argNum}** was missing. It is supposed to be **${desc}**`);
+  if (message.channel instanceof TextChannel && !message.channel.permissionsFor(message.guild.me).has('EMBED_LINKS')) return void message.channel.send(`:x: Argument **#${argNum}** was missing. It is supposed to be **${desc}**`);
 
   const embed = new MessageEmbed()
     .setColor(embedColor)
