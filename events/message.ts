0e9bdb04ecf0e16c0de9a388751bb097ab7be513
Jyguy <jskekkx@gmail.com>
2019-09-18 16:00:06 -0400

    another fix for dm commands not working

--- events/message.ts
+++ events/message.ts
@@ -2,12 +2,12 @@ import ReknownClient from '../structures/client';
 import { Message } from 'discord.js';
 
 module.exports.run = async (client: ReknownClient, message: Message): Promise<void> => {
-  if (message.author.bot || !message.guild.available) return;
+  if (message.author.bot || message.guild && !message.guild.available) return;
   if (message.member.partial) await message.member.fetch();
 
   const prefix = message.guild ? await client.functions.getPrefix(client, message.guild.id) : client.config.prefix;
   const regexp = new RegExp(`^<@!?${message.client.user.id}> `);
-  if ((!message.content.startsWith(prefix) && !message.content.match(regexp)) || message.content === prefix) return;
+  if (!message.content.startsWith(prefix) && !message.content.match(regexp) || message.content === prefix) return;
 
   let args: string[];
   if (message.content.match(regexp)) args = message.content.slice(message.content.match(regexp)[0].length).split(/ +/g);
