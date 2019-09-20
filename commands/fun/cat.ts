a01c2da576c0cfb4d451be46e66719d2a11cd76d
Jyguy <jskekkx@gmail.com>
2019-09-20 15:55:34 -0400

    Added ?cat and ?dog.

---
+++ commands/fun/cat.ts
@@ -0,0 +1,27 @@
+import { ReknownClient } from "ReknownBot";
+import { Message, MessageEmbed } from "discord.js";
+import fetch from 'node-fetch';
+
+interface CatResult {
+  file: string;
+}
+
+module.exports.run = async (client: ReknownClient, message: Message, args: string[]) => {
+  const json: CatResult = await fetch('https://aws.random.cat/meow').then(res => res.json());
+  if (!json || !json.file) return message.reply('Seems like the API is down, please try again later. If this problem persists, let us know in our Discord server.');
+
+  const embed = new MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setFooter(`Requested by ${message.author.tag} | Powered by https://aws.random.cat/`, message.author.displayAvatarURL())
+    .setImage(json.file)
+    .setTitle('Kitty!');
+
+  message.channel.send(embed);
+};
+
+module.exports.help = {
+  aliases: [],
+  category: 'Fun',
+  desc: 'Shows a picture of a cat.',
+  usage: 'cat'
+};
---
+++ commands/fun/dog.ts
@@ -0,0 +1,28 @@
+import { ReknownClient } from "ReknownBot";
+import { Message, MessageEmbed } from "discord.js";
+import fetch from 'node-fetch';
+
+interface DogResult {
+  message: string;
+  status: string;
+}
+
+module.exports.run = async (client: ReknownClient, message: Message, args: string[]) => {
+  const json: DogResult = await fetch('https://dog.ceo/api/breeds/image/random').then(res => res.json());
+  if (!json || json.status !== 'success') return message.reply('Seems like the API is down, please try again later. If this problem persists, let us know in our Discord server.');
+
+  const embed = new MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setFooter(`Requested by ${message.author.tag} | Powered by https://dog.ceo/dog-api`, message.author.displayAvatarURL())
+    .setImage(json.message)
+    .setTitle('Doggo!');
+
+  message.channel.send(embed);
+};
+
+module.exports.help = {
+  aliases: [],
+  category: 'Fun',
+  desc: 'Shows a picture of a dog.',
+  usage: 'dog'
+};
