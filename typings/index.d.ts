d8dcc7c02c85c023cc4b2160e3eb7ddbe9c7559e
Jyguy <jskekkx@gmail.com>
2019-09-21 15:10:40 -0400

    better music POG

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,11 +1,11 @@
 declare module 'ReknownBot' {
-  import { Message, Snowflake, GuildChannel, Guild, GuildMember, Role, User, MessageEmbed, TextChannel, VoiceChannel, CategoryChannel, VoiceConnection } from 'discord.js';
-  import { Player } from 'discord.js-lavalink';
+  import { Message, Snowflake, GuildChannel, Guild, GuildMember, Role, User, MessageEmbed, TextChannel, VoiceChannel, CategoryChannel, ClientUser } from 'discord.js';
+  import { Player, Track } from 'lavalink';
   type ReknownClient = import('../structures/client').default;
 
   export class ReknownFunctions {
     public badArg(message: Message, argNum: number, desc: string): void;
-    public endSession(music: MusicObject, client: ReknownClient): void;
+    public endSession(music: MusicObject): void;
     public formatNum(num: number): string;
     public getPrefix(client: ReknownClient, id: Snowflake): Promise<string>;
     public noArg(message: Message, argNum: number, desc: string): void;
@@ -17,8 +17,9 @@ declare module 'ReknownBot' {
     public parseMention(id: Snowflake, guild: Guild, options: ParseMentionOptions & { type: 'channel'; cType?: 'text' }): TextChannel | null;
     public parseMention(id: Snowflake, guild: Guild, options: ParseMentionOptions & { type: 'channel'; cType?: 'voice' }): VoiceChannel | null;
     public parseMention(id: Snowflake, guild: Guild, options: ParseMentionOptions & { type: 'channel'; cType?: 'category' }): CategoryChannel | null;
-    public playMusic(client: ReknownClient, guild: Guild, vc: VoiceChannel, music: MusicObject, track: string): void;
+    public playMusic(client: ReknownClient, guild: Guild, vc: VoiceChannel, music: MusicObject, track: string, ended?: boolean): void;
     public sendLog(client: ReknownClient, embed: MessageEmbed, guild: Guild): Promise<void>;
+    public sendSong(music: MusicObject, message: Message, song: Track, user: ClientUser): void;
     public uppercase(str: string): string;
   }
 
