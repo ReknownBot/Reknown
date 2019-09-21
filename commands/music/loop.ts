5fde286059162942f597b9046a63bde0f74ecc8a
Jyguy <jskekkx@gmail.com>
2019-09-21 17:20:47 -0400

    Added ?loop and fixed some music issues

---
+++ commands/music/loop.ts
@@ -0,0 +1,20 @@
+import { ReknownClient } from "ReknownBot";
+import { Message, DMChannel } from "discord.js";
+
+module.exports.run = (client: ReknownClient, message: Message, args: string[]) => {
+  if (message.channel instanceof DMChannel) return message.reply(':x: This command is only available in servers.');
+
+  const music = client.music[message.guild.id];
+  const bool = args[1] ? args[1].toLowerCase() === 'on' : !music.looping;
+  if (!bool && args[1].toLowerCase() !== 'off') return client.functions.badArg(message, 1, 'This must be either "on" or "off".');
+  music.looping = bool;
+
+  message.channel.send(`Successfully toggled looping ${bool ? 'on' : 'off'}.`);
+};
+
+module.exports.help = {
+  aliases: [],
+  category: 'Music',
+  desc: 'Toggles looping for music.',
+  usage: 'loop [Toggle=on]'
+};
