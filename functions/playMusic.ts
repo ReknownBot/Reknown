2a977bbe62380dc3234d517f34457d38d921f03c
Jyguy <jskekkx@gmail.com>
2019-09-21 21:37:25 -0400

    another music fix thingy

--- functions/playMusic.ts
+++ functions/playMusic.ts
@@ -2,7 +2,10 @@ import { Guild, VoiceChannel } from 'discord.js';
 import { ReknownClient, MusicObject } from 'ReknownBot';
 
 module.exports = async (client: ReknownClient, guild: Guild, vc: VoiceChannel, music: MusicObject, track: string, ended?: boolean) => {
-  if (!ended && music.queue.length > 0) return music.queue.push(track);
+  if (!ended) {
+    music.queue.push(track);
+    if (music.queue.length > 0) return;
+  }
 
   music.player = client.lavalink.players.get(guild.id);
   await music.player.join(vc.id);
