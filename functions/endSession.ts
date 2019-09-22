5f923d57528b63f919fc05344b2b302fe1a98c6e
Jyguy <jskekkx@gmail.com>
2019-09-21 22:12:41 -0400

    i guess im trying

--- functions/endSession.ts
+++ functions/endSession.ts
@@ -4,6 +4,5 @@ module.exports = (music: MusicObject) => {
   music.queue = [];
   music.looping = false;
   music.volume = 20;
-  music.player.stop();
   music.player.leave();
 };
--- functions/playMusic.ts
+++ functions/playMusic.ts
@@ -11,14 +11,11 @@ module.exports = async (client: ReknownClient, guild: Guild, music: MusicObject,
 
   music.player.once('event', async d => {
     if (d.reason === 'REPLACED') return;
-    if (!guild.voice.channel || d.reason === 'STOPPED') return client.functions.endSession(music);
+    if (!guild.voice.channel) return client.functions.endSession(music);
     if (music.looping) music.queue.push(music.queue.shift());
     else music.queue.shift();
 
-    if (music.queue.length > 0) {
-      await music.player.stop();
-      return module.exports(client, guild, music, music.queue[0], true);
-    }
+    if (music.queue.length > 0) return setTimeout(module.exports, 500, client, guild, music, music.queue[0], true);
     client.functions.endSession(music);
   });
 };
