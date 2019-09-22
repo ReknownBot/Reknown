a8ef5d9c382ab53f323264f49fa0bcd0c3dc216b
Jyguy <jskekkx@gmail.com>
2019-09-21 21:57:29 -0400

    trying to fix

--- functions/playMusic.ts
+++ functions/playMusic.ts
@@ -9,13 +9,16 @@ module.exports = async (client: ReknownClient, guild: Guild, music: MusicObject,
 
   music.player.play(track);
 
-  music.player.once('event', d => {
+  music.player.once('event', async d => {
     if (d.reason === 'REPLACED') return;
     if (!guild.voice.channel || d.reason === 'STOPPED') return client.functions.endSession(music);
     if (music.looping) music.queue.push(music.queue.shift());
     else music.queue.shift();
 
-    if (music.queue.length > 0) return module.exports(client, guild, music, music.queue[0], true);
+    if (music.queue.length > 0) {
+      await music.player.stop();
+      return module.exports(client, guild, music, music.queue[0], true);
+    }
     client.functions.endSession(music);
   });
 };
