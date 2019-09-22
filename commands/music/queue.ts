7e4d0a1a5dfaf44d3d151a339c364ba5cf3b7d16
Jyguy <jskekkx@gmail.com>
2019-09-21 23:43:04 -0400

    Fix queue formatting

--- commands/music/queue.ts
+++ commands/music/queue.ts
@@ -9,7 +9,7 @@ module.exports.run = (client: ReknownClient, message: Message, args: string[]) =
 
   let msg: MessageEmbed | string;
   if (message.channel.permissionsFor(client.user).has('EMBED_LINKS')) {
-    let desc = music.queue.map((song, i) => `${i + 1}. [\`${client.escMD(song.info.title)}\`](${client.escMD(song.info.uri)})`).join(' ');
+    let desc = music.queue.map((song, i) => `${i + 1}. [\`${client.escMD(song.info.title)}\`](${client.escMD(song.info.uri)})`).join('\n');
     if (desc.length > 2048) desc = `${desc.slice(0, -3)}...`;
 
     msg = new MessageEmbed()
@@ -18,7 +18,7 @@ module.exports.run = (client: ReknownClient, message: Message, args: string[]) =
       .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
       .setTitle('Current Queue');
   } else {
-    msg = music.queue.map((song, i) => `${i + 1}. \`${client.escMD(song.info.title)}\``).join(' ');
+    msg = music.queue.map((song, i) => `${i + 1}. \`${client.escMD(song.info.title)}\``).join('\n');
     if (msg.length > 2048) msg = `${msg.slice(0, -3)}...`;
   }
 
