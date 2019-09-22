e03778ee40cf0cd920daca4013d67177c60a09a8
Jyguy <jskekkx@gmail.com>
2019-09-22 13:26:06 -0400

    fix(loop): err when running without music initialized

--- commands/music/loop.ts
+++ commands/music/loop.ts
@@ -4,7 +4,13 @@ import { Message, DMChannel } from 'discord.js';
 module.exports.run = (client: ReknownClient, message: Message, args: string[]) => {
   if (message.channel instanceof DMChannel) return message.reply(':x: This command is only available in servers.');
 
-  const music = client.music[message.guild.id];
+  let music = client.music[message.guild.id];
+  if (!music) music = client.music[message.guild.id] = {
+    looping: false,
+    player: null,
+    queue: [],
+    volume: 20
+  };
   const bool = args[1] ? args[1].toLowerCase() === 'on' : !music.looping;
   if (!bool && args[1] && args[1].toLowerCase() !== 'off') return client.functions.badArg(message, 1, 'This must be either "on" or "off".');
   music.looping = bool;
