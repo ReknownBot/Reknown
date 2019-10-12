1dad0246acfd4a5d1490e9f6fe3681115027aa82
Jyguy <jskekkx@gmail.com>
2019-10-12 02:01:26 -0400

    hopefully this fixes update row

--- functions/updateRow.ts
+++ functions/updateRow.ts
@@ -6,6 +6,6 @@ module.exports = async (client: ReknownClient, table: string, changes: { [ colum
   const fColumns = Object.keys(filters);
   const fValues = Object.values(filters);
   const row = (await client.query(`SELECT * FROM ${table}`)).rows[0];
-  if (row) client.query(`UPDATE ${table} SET ${columns.map((c, i) => `${c} = $${i + 1}`).join(', ')} WHERE ${fColumns.map((c, i) => `${c} = $${i + columns.length + 1}`)}`, [ ...values, ...fValues ]);
+  if (row) client.query(`UPDATE ${table} SET ${columns.map((c, i) => `${c} = $${i + 1}`).join(', ')} WHERE ${fColumns.map((c, i) => `${c} = $${i + columns.length + 1}`).join(' AND ')}`, [ ...values, ...fValues ]);
   else client.query(`INSERT INTO ${table} (${columns.join(', ')}) VALUES (${columns.map((c, i) => `$${i + 1}`)})`, values);
 };
