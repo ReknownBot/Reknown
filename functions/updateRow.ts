c1bb61e70212975d8234e94572882d2f67e78027
Jyguy <jskekkx@gmail.com>
2019-10-12 02:04:41 -0400

    probably fix for updateRow

--- functions/updateRow.ts
+++ functions/updateRow.ts
@@ -3,9 +3,8 @@ import { ReknownClient } from 'ReknownBot';
 module.exports = async (client: ReknownClient, table: string, changes: { [ column: string ]: any }, filters: { [ column: string ]: any }) => {
   const columns = Object.keys(changes);
   const values = Object.values(changes);
-  const fColumns = Object.keys(filters);
-  const fValues = Object.values(filters);
-  const row = (await client.query(`SELECT * FROM ${table}`)).rows[0];
-  if (row) client.query(`UPDATE ${table} SET ${columns.map((c, i) => `${c} = $${i + 1}`).join(', ')} WHERE ${fColumns.map((c, i) => `${c} = $${i + columns.length + 1}`).join(' AND ')}`, [ ...values, ...fValues ]);
+  const filter = Object.keys(filters).map((c, i) => `${c} = $${i + columns.length + 1}`).join(' AND ');
+  const row = (await client.query(`SELECT * FROM ${table} WHERE ${filter}`)).rows[0];
+  if (row) client.query(`UPDATE ${table} SET ${columns.map((c, i) => `${c} = $${i + 1}`).join(', ')} WHERE ${filter}`, [ ...values, ...Object.values(filters) ]);
   else client.query(`INSERT INTO ${table} (${columns.join(', ')}) VALUES (${columns.map((c, i) => `$${i + 1}`)})`, values);
 };
