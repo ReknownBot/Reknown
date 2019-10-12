e780496fe290e4bbf1d8c547621cb4629be8e4d1
Jyguy <jskekkx@gmail.com>
2019-10-12 01:56:54 -0400

    added a better config function

--- commands/misc/prefix.ts
+++ commands/misc/prefix.ts
@@ -1,6 +1,5 @@
 import ReknownClient from '../../structures/client';
 import { Message, DMChannel } from 'discord.js';
-import { PrefixRow } from 'ReknownBot';
 
 module.exports.run = async (client: ReknownClient, message: Message, args: string[]) => {
   if (message.channel instanceof DMChannel) return message.reply(':x: This command is only available in servers.');
@@ -13,9 +12,12 @@ module.exports.run = async (client: ReknownClient, message: Message, args: strin
   if (!message.member!.hasPermission('ADMINISTRATOR')) return client.functions.noPerms(message, [ 'Administrator' ]);
   const prefix = args.slice(1).join(' ');
   if (prefix.length > 15) return client.functions.badArg(message, 1, 'The prefix length must be lower than 16.');
-  const row: PrefixRow = (await client.query('SELECT * FROM prefix WHERE guildid = $1', [ message.guild!.id ])).rows[0];
-  if (!row) client.query('INSERT INTO prefix (customprefix, guildid) VALUES ($1, $2)', [ prefix, message.guild!.id ]);
-  else client.query('UPDATE prefix SET customprefix = $1 WHERE guildid = $2', [ prefix, message.guild!.id ]);
+  client.functions.updateRow(client, 'prefix', {
+    customprefix: prefix,
+    guildid: message.guild!.id
+  }, {
+    guildid: message.guild!.id
+  });
 
   message.channel.send(`Successfully updated the prefix to \`${client.escMD(prefix)}\`.`);
 };
