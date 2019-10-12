71ac37b0b07a73e8badfea2709d95089508c9fc5
Jyguy <jskekkx@gmail.com>
2019-10-12 16:41:00 -0400

    fix compile error

--- functions/getPrefix.ts
+++ functions/getPrefix.ts
@@ -1,8 +1,8 @@
 import ReknownClient from '../structures/client';
 import { Snowflake } from 'discord.js';
-import { PrefixRow } from 'ReknownBot';
 
 module.exports = async (client: ReknownClient, id: Snowflake): Promise<string> => {
-  const row: PrefixRow = (await client.query('SELECT customprefix FROM prefix WHERE guildid = $1', [ id ])).rows[0];
-  return row ? row.customprefix : client.config.prefix;
+  if (client.prefixes[id]) return client.prefixes[id];
+  const row = (await client.query('SELECT customprefix FROM prefix WHERE guildid = $1', [ id ])).rows[0];
+  return client.prefixes[id] = row ? row.customprefix : client.config.prefix;
 };
