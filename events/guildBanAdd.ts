409c598c6caa195b5ca1bd9e516ad8aeac25057e
Jyguy <jskekkx@gmail.com>
2019-10-13 21:26:37 -0400

    added guildBanAdd and guildBanRemove events

---
+++ events/guildBanAdd.ts
@@ -0,0 +1,25 @@
+import ReknownClient from '../structures/client';
+import { MessageEmbed, User, Guild, PartialUser } from 'discord.js';
+
+async function sendLog (client: ReknownClient, guild: Guild, user: User) {
+  const bans = await guild.fetchBans();
+  const ban = bans.find(ban => ban.user.id === user.id)!;
+
+  const embed = new MessageEmbed()
+    .addField('User', user.tag)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${user.id}`)
+    .setThumbnail(user.displayAvatarURL({ size: 512 }))
+    .setTimestamp()
+    .setTitle('Member Banned');
+  if (ban.reason) embed.addField('Reason', ban.reason);
+
+  client.functions.sendLog(client, embed, guild);
+}
+
+module.exports.run = async (client: ReknownClient, guild: Guild, user: User) => {
+  if (!guild.available) return;
+  if (user.id === client.user!.id) return;
+
+  sendLog(client, guild, user);
+};
