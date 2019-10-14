3461a1ee8bef09caae5c0a77b57f99845c5a7a59
Jyguy <jskekkx@gmail.com>
2019-10-14 17:26:27 -0400

    added ?cowsay and ?cowthink

---
+++ commands/fun/cowthink.ts
@@ -0,0 +1,17 @@
+import { ReknownClient } from "ReknownBot";
+import { Message } from "discord.js";
+import { think } from 'cowsay';
+
+module.exports.run = (client: ReknownClient, message: Message, args: string[]) => {
+  if (!args[1]) return client.functions.noArg(message, 1, 'a message for a cow to think about.');
+  const msg = args.slice(1).join(' ');
+
+  return message.channel.send(`\`\`\`${think({ text: msg })}\`\`\``);
+};
+
+module.exports.help = {
+  aliases: [],
+  category: 'Fun',
+  desc: 'Makes a cow think of something.',
+  usage: 'cowthink <Message>'
+};
