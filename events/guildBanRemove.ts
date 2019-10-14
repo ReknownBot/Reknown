974c69f890857be0c9be1f666f661ebf446078a3
Jyguy <jskekkx@gmail.com>
2019-10-13 21:26:37 -0400

    added guildBanAdd and guildBanRemove events

---
+++ events/guildBanRemove.ts
@@ -0,0 +1,22 @@
+import ReknownClient from '../structures/client';
+import { MessageEmbed, User, Guild, PartialUser } from 'discord.js';
+
+function sendLog (client: ReknownClient, guild: Guild, user: User) {
+  const embed = new MessageEmbed()
+    .addField('User', user.tag)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${user.id}`)
+    .setThumbnail(user.displayAvatarURL({ size: 512 }))
+    .setTimestamp()
+    .setTitle('Member Unbanned');
+
+  client.functions.sendLog(client, embed, guild);
+}
+
+module.exports.run = async (client: ReknownClient, guild: Guild, user: User | PartialUser) => {
+  if (!guild.available) return;
+  if (user.id === client.user!.id) return;
+  if (user.partial) user = await user.fetch();
+
+  sendLog(client, guild, user);
+};
