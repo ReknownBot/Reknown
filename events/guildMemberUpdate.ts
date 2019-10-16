34df37231a4b93d319e675ab387de34025b8394c
Jyguy <jskekkx@gmail.com>
2019-10-16 16:39:51 -0400

    added member role update logs

--- events/guildMemberUpdate.ts
+++ events/guildMemberUpdate.ts
@@ -16,8 +16,25 @@ function nickUpdate (client: ReknownClient, oldMember: GuildMember, newMember: G
   client.functions.sendLog(client, embed, newMember.guild);
 }
 
+function roleUpdate (client: ReknownClient, oldMember: GuildMember, newMember: GuildMember) {
+  const addedRoles = newMember.roles.filter(r => !oldMember.roles.has(r.id));
+  const removedRoles = oldMember.roles.filter(r => !newMember.roles.has(r.id));
+  if (addedRoles.size === 0 && removedRoles.size === 0) return;
+
+  const embed = new MessageEmbed()
+    .addField('Member', newMember.user.tag)
+    .addField('Roles Changed', `${addedRoles.map(r => `+ \`${client.escMD(r.name)}\``)}\n${removedRoles.map(r => `- \`${client.escMD(r.name)}\``)}`)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${newMember.id}`)
+    .setTimestamp()
+    .setTitle('Member Role Update');
+
+  client.functions.sendLog(client, embed, newMember.guild);
+}
+
 module.exports.run = (client: ReknownClient, oldMember: GuildMember, newMember: GuildMember) => {
   if (!newMember.guild.available) return;
 
   nickUpdate(client, oldMember, newMember);
+  roleUpdate(client, oldMember, newMember);
 };
