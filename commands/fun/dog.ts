10f2db1bbc10014dd996f68f6336c0ce524a4e34
Jyguy <jskekkx@gmail.com>
2019-10-16 20:34:31 -0400

    added missing embed perms checks

--- commands/fun/dog.ts
+++ commands/fun/dog.ts
@@ -1,5 +1,5 @@
 import { ReknownClient } from 'ReknownBot';
-import { Message, MessageEmbed } from 'discord.js';
+import { Message, MessageEmbed, TextChannel } from 'discord.js';
 import fetch from 'node-fetch';
 
 interface DogResult {
@@ -8,6 +8,8 @@ interface DogResult {
 }
 
 module.exports.run = async (client: ReknownClient, message: Message, args: string[]) => {
+  if (message.channel instanceof TextChannel && !message.channel.permissionsFor(client.user!)!.has('EMBED_LINKS')) return client.functions.noClientPerms(message, [ 'Embed Links' ], message.channel);
+
   const json: DogResult = await fetch('https://dog.ceo/api/breeds/image/random').then(res => res.json());
   if (!json || json.status !== 'success') return message.reply('Seems like the API is down, please try again later. If this problem persists, let us know in our Discord server.');
 
