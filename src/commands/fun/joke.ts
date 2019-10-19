a7e24c8a30805762b1d0cecc9884efcfa05d0ca8
Jyguy <jskekkx@gmail.com>
2019-10-19 12:33:46 -0400

    fix(joke): not responding

--- src/commands/fun/joke.ts
+++ src/commands/fun/joke.ts
@@ -9,7 +9,9 @@ interface JokeResult {
 }
 
 module.exports.run = async (client: ReknownClient, message: Message, args: string[]) => {
-  const json: JokeResult = await fetch('https://icanhazdadjoke.com/').then(res => res.json());
+  const json: JokeResult = await fetch('https://icanhazdadjoke.com/', { headers: {
+    Accept: 'application/json'
+  } }).then(res => res.json());
   if (!json.joke) return message.reply('Seems like the joke API is unavailable, please try again later.');
 
   message.channel.send(json.joke);
