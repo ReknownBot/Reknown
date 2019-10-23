51062eb6803e924cf64c2ae6421a9b76718e6324
Jyguy <jskekkx@gmail.com>
2019-10-23 14:37:08 -0400

    fix(music): pausing identifies as not playing anything

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -6,7 +6,7 @@ module.exports.run = (client: ReknownClient, message: Message, args: string[]) =
 
   const music = client.music[message.guild!.id];
 
-  if (!music || !music.player || !music.player.playing) return message.reply('I am not playing anything!.');
+  if (!music || !music.player || (!music.player.playing && !music.player.paused)) return message.reply('I am not playing anything!.');
   if (!music.player.paused) return message.reply('The music is not paused.');
   if (message.guild!.voice!.channelID !== message.member!.voice.channelID) return message.reply('You must be in the same voice channel as me to run that command.');
 
