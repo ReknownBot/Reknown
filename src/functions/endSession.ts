b122d72900e3b00be3bffe03d494fb2d1826b15a
Jyguy <jskekkx@gmail.com>
2019-10-23 15:38:52 -0400

    remove config reset after song transition or stop

--- src/functions/endSession.ts
+++ src/functions/endSession.ts
@@ -2,8 +2,6 @@ import { MusicObject } from 'ReknownBot';
 
 module.exports = (music: MusicObject) => {
   music.queue = [];
-  music.looping = false;
-  music.volume = 20;
   music.player!.leave();
   music.player!.stop();
 };
