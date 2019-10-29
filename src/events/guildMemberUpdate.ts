f0b4d400ec119f7c7b73c7a6906a184a7e6088ac
Jyguy <jskekkx@gmail.com>
2019-10-29 18:25:45 -0400

    logs: added avatar and username logs

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,5 +1,20 @@
 import ReknownClient from '../structures/client';
-import { GuildMember, MessageEmbed } from 'discord.js';
+import { Guild, GuildMember, MessageEmbed, User } from 'discord.js';
+
+function avatarUpdate (client: ReknownClient, oldUser: User, newUser: User, guild: Guild) {
+  if (oldUser.displayAvatarURL() === newUser.displayAvatarURL()) return;
+
+  const embed = new MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setDescription(`[Old Avatar](${oldUser.displayAvatarURL({ size: 2048 })}) => [New Avatar](${newUser.displayAvatarURL({ size: 2048 })})`)
+    .setFooter(`ID: ${newUser.id} | Tip: The larger one is the new one!`)
+    .setImage(newUser.displayAvatarURL({ size: 2048 }))
+    .setThumbnail(oldUser.displayAvatarURL({ size: 2048 }))
+    .setTimestamp()
+    .setTitle('Username Changed');
+
+  client.functions.sendLog(client, embed, guild);
+}
 
 function nickUpdate (client: ReknownClient, oldMember: GuildMember, newMember: GuildMember) {
   if (oldMember.displayName === newMember.displayName) return;
@@ -34,9 +49,25 @@ function roleUpdate (client: ReknownClient, oldMember: GuildMember, newMember: G
   client.functions.sendLog(client, embed, newMember.guild);
 }
 
+function usernameUpdate (client: ReknownClient, oldUser: User, newUser: User, guild: Guild) {
+  if (oldUser.username === newUser.username) return;
+
+  const embed = new MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setDescription(`\`\`${client.escInline(oldUser.username)}\`\` => \`\`${client.escInline(newUser.username)}\`\``)
+    .setFooter(`ID: ${newUser.id}`)
+    .setThumbnail(newUser.displayAvatarURL({ size: 512 }))
+    .setTimestamp()
+    .setTitle('Username Changed');
+
+  client.functions.sendLog(client, embed, guild);
+}
+
 module.exports.run = (client: ReknownClient, oldMember: GuildMember, newMember: GuildMember) => {
   if (!newMember.guild.available) return;
 
+  avatarUpdate(client, oldMember.user, newMember.user, newMember.guild);
   nickUpdate(client, oldMember, newMember);
   roleUpdate(client, oldMember, newMember);
+  usernameUpdate(client, oldMember.user, newMember.user, newMember.guild);
 };
