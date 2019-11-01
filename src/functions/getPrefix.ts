b18a9ab6aec452049f0e56fa8ec03f74306f5ff4
Jyguy <jskekkx@gmail.com>
2019-11-01 16:27:16 -0400

    added ?balance and simplified row fetching

--- src/functions/getPrefix.ts
+++ src/functions/getPrefix.ts
@@ -3,6 +3,8 @@ import { Snowflake } from 'discord.js';
 
 export async function run (client: ReknownClient, id: Snowflake): Promise<string> {
   if (client.prefixes[id]) return client.prefixes[id];
-  const row = (await client.query('SELECT customprefix FROM prefix WHERE guildid = $1', [ id ])).rows[0];
+  const row = await client.functions.getRow(client, 'prefix', {
+    guildid: id
+  });
   return client.prefixes[id] = row ? row.customprefix : client.config.prefix;
 }
---
+++ src/functions/getRow.ts
@@ -0,0 +1,7 @@
+import { ReknownClient } from "ReknownBot";
+
+export async function run (client: ReknownClient, table: string, filters: { [ rowName: string ]: any }) {
+  const query = `SELECT * FROM ${table} WHERE ${Object.keys(filters).map((rowName, i) => `${rowName} = $${i + 1}`).join(' AND ')}`;
+  const { rows } = await client.query(query, Object.values(filters));
+  return rows ? rows[0] : null;
+}
