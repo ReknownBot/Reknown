ff7aaddeb69d17912aaaacd84e0e5731c4127365
Jyguy <jskekkx@gmail.com>
2019-11-02 14:41:39 -0400

    added ?work and Constants

--- src/functions/getPrefix.ts
+++ src/functions/getPrefix.ts
@@ -1,9 +1,10 @@
 import ReknownClient from '../structures/client';
 import { Snowflake } from 'discord.js';
+import { tables } from '../Constants';
 
 export async function run (client: ReknownClient, id: Snowflake): Promise<string> {
   if (client.prefixes[id]) return client.prefixes[id];
-  const row = await client.functions.getRow(client, 'prefix', {
+  const row = await client.functions.getRow(client, tables.PREFIX, {
     guildid: id
   });
   return client.prefixes[id] = row ? row.customprefix : client.config.prefix;
