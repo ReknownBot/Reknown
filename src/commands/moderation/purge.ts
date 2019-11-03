fa075ac718c2291ecdef0146422d7a499207ab8d
Jyguy <jskekkx@gmail.com>
2019-11-03 01:18:16 -0500

    added ?purge

---
+++ src/commands/moderation/purge.ts
@@ -0,0 +1,21 @@
+import { ReknownClient } from 'ReknownBot';
+import { Message, TextChannel } from 'discord.js';
+
+export async function run (client: ReknownClient, message: Message & { channel: TextChannel }, args: string[]) {
+  if (!message.channel.permissionsFor(message.member!)!.has('MANAGE_MESSAGES')) return client.functions.noPerms(message, [ 'Manage Messages' ], message.channel);
+  if (!message.channel.permissionsFor(client.user!)!.has('MANAGE_MESSAGES')) return client.functions.noPerms(message, [ 'Manage Messages' ], message.channel);
+
+  if (!args[1]) return client.functions.noArg(message, 1, 'an amount to purge.');
+  const amt = parseInt(args[1]);
+  if (amt < 1 || amt > 100) return client.functions.badArg(message, 1, 'The amount must be from 1-100.');
+
+  if (amt === 100) await message.delete();
+  message.channel.bulkDelete(amt === 100 ? amt : amt + 1);
+}
+
+export const help = {
+  aliases: [ 'clear' ],
+  category: 'Moderation',
+  desc: 'Clears x amount of messages.',
+  usage: 'purge <Amount>'
+};
