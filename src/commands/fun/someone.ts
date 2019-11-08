52fef9841c0fc4df461fdc81d7717cf43fe6bf05
Jyguy <jskekkx@gmail.com>
2019-11-07 20:43:12 -0500

    added ?someone

---
+++ src/commands/fun/someone.ts
@@ -0,0 +1,18 @@
+import { HelpObj } from 'ReknownBot';
+import ReknownClient from '../../structures/client';
+import { Message, TextChannel } from 'discord.js';
+
+export async function run (client: ReknownClient, message: Message & { channel: TextChannel }, args: string[]) {
+  let members = message.guild!.members;
+  if (members.size !== message.guild!.memberCount) members = await message.guild!.members.fetch();
+
+  const member = members.filter(m => !m.user.bot).random();
+  message.channel.send(`Successfully randomly fetched **${client.escMD(member.user.tag)}**!`);
+}
+
+export const help: HelpObj = {
+  aliases: [ 'randomuser' ],
+  category: 'Fun',
+  desc: 'Gets a random member in a server.',
+  usage: 'someone'
+};
