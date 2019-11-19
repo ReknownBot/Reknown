341b95ec5831ad17df909c0f428a35e165963b6c
Jyguy <jskekkx@gmail.com>
2019-11-19 17:30:17 -0500

    fix compiling error

--- src/events/roleCreate.ts
+++ src/events/roleCreate.ts
@@ -1,12 +1,12 @@
 import { ReknownClient } from 'ReknownBot';
-import { MessageEmbed, PermissionString, Role } from 'discord.js';
+import { MessageEmbed, Role } from 'discord.js';
 
 function sendLog (client: ReknownClient, role: Role) {
-  const permissions = role.permissions.serialize(false);
+  // const permissions = role.permissions.serialize(false);
 
   const embed = new MessageEmbed()
     .addField('Role Name', client.escMD(role.name))
-    .addField('Permissions', Object.keys(permissions).map(perm => `${perm}: ${permissions[perm as PermissionString]}`).join('\n'))
+    // .addField('Permissions', Object.keys(permissions).map(perm => `${perm}: ${permissions[perm as PermissionString]}`).join('\n'))
     .setColor(client.config.embedColor)
     .setFooter(`ID: ${role.id}`)
     .setTimestamp()
