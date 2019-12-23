d09bec3309338057ae37d8e5060ee0747eb5cc46
Jyguy <jskekkx@gmail.com>
2019-12-22 23:28:52 -0500

    added commandhandler structure

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,9 @@ const client = new ReknownClient({
   disableEveryone: true,
   partials: [ 'GUILD_MEMBER', 'MESSAGE', 'USER' ]
 });
-const categories = readdirSync('./dist/commands');
-categories.forEach(name => readdirSync(`./dist/commands/${name}`).forEach(f => client.commands.set(f.slice(0, -3), require(`./commands/${name}/${f}`))));
+
 const eventList = readdirSync('./dist/events');
 eventList.forEach(f => client.events.set(f.slice(0, -3), require(`./events/${f}`)));
-client.commands.forEach((obj, name) => {
-  obj.help.aliases.forEach(alias => {
-    client.aliases[alias] = name;
-  });
-  client.aliases[name] = name;
-});
-client.categories = [ ...new Set(client.commands.map(c => c.help.category)) ];
-
 client.events.forEach((obj, name) => client.on(name, (...args: any) => obj.run(client, ...args)));
 
 process.on('unhandledRejection', console.log);
---
+++ src/structures/commandhandler.ts
@@ -0,0 +1,28 @@
+import { Collection } from 'discord.js';
+import { readdirSync } from 'fs';
+import { CommandCategory, ReknownCommand } from 'ReknownBot';
+
+export default class CommandHandler extends Collection<string, ReknownCommand> {
+  public constructor () {
+    super();
+
+    this.rawCategories = readdirSync('./dist/commands');
+    this.rawCategories.forEach(c => readdirSync(`./dist/commands/${c}`).forEach(cmd => {
+      this.list.push(cmd.slice(0, -3));
+      const command: ReknownCommand = require(`../commands/${c}/${cmd}`); // eslint-disable-line @typescript-eslint/no-var-requires, global-require
+      this.set(cmd.slice(0, -3), command);
+      command.help.aliases.forEach(alias => this.aliases[alias] = cmd.slice(0, -3));
+      this.aliases[cmd.slice(0, -3)] = cmd.slice(0, -3);
+
+      if (!this.categories.includes(command.help.category)) this.categories.push(command.help.category);
+    }));
+  }
+
+  public aliases: { [ alias: string ]: string } = {};
+
+  public categories: CommandCategory[] = [];
+
+  public list: string[] = [];
+
+  public rawCategories: string[] = [];
+}
