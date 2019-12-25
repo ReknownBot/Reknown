ce9d13e492f291ceb444d12238b30e2401129eb4
Jyguy <jskekkx@gmail.com>
2019-12-25 18:35:11 -0500

    added channel creation logs

---
+++ src/events/channelCreate.ts
@@ -0,0 +1,20 @@
+import { ReknownClient } from 'ReknownBot';
+import { GuildChannel, MessageEmbed } from 'discord.js';
+
+async function sendLog (client: ReknownClient, channel: GuildChannel) {
+  const embed = new MessageEmbed()
+    .addField('Channel', channel.toString())
+    .addField('Channel Type', channel.type)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${channel.id}`)
+    .setTimestamp()
+    .setTitle('Channel Created');
+
+  client.functions.sendLog(client, embed, channel.guild);
+}
+
+export async function run (client: ReknownClient, channel: GuildChannel) {
+  if (!channel.guild.available) return;
+
+  sendLog(client, channel);
+}
