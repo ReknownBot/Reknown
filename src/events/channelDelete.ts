1c1254a5f85ec81e8d9f53b3cf705590aa6da1cb
Jyguy <jskekkx@gmail.com>
2019-12-26 16:29:34 -0500

    added channel deletion logs

---
+++ src/events/channelDelete.ts
@@ -0,0 +1,22 @@
+import ReknownClient from '../structures/client';
+import { GuildChannel, MessageEmbed } from 'discord.js';
+
+async function sendLog (client: ReknownClient, channel: GuildChannel) {
+  const embed = new MessageEmbed()
+    .addField('Channel', channel.toString())
+    .addField('Channel Type', channel.type)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${channel.id}`)
+    .setTimestamp()
+    .setTitle('Channel Deleted');
+
+  if (channel.parent) embed.addField('Category', client.escMD(channel.parent.name));
+
+  client.functions.sendLog(client, embed, channel.guild);
+}
+
+export async function run (client: ReknownClient, channel: GuildChannel) {
+  if (!channel.guild.available) return;
+
+  sendLog(client, channel);
+}
