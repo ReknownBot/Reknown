888dc07ab5039012343daa4e9ba24906377215b3
Jyguy <jskekkx@gmail.com>
2020-01-01 03:16:06 -0500

    added emoji deletion logs

---
+++ src/events/emojiDelete.ts
@@ -0,0 +1,21 @@
+import ReknownClient from '../structures/client';
+import { GuildEmoji, MessageEmbed } from 'discord.js';
+
+async function sendLog (client: ReknownClient, emoji: GuildEmoji) {
+  const embed = new MessageEmbed()
+    .addField('Emoji Name', client.escMD(emoji.name))
+    .addField('Animated', emoji.animated ? 'Yes' : 'No')
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${emoji.id}`)
+    .setThumbnail(emoji.url!)
+    .setTimestamp()
+    .setTitle('Emoji Deleted');
+
+  client.functions.sendLog(client, embed, emoji.guild);
+}
+
+export async function run (client: ReknownClient, emoji: GuildEmoji) {
+  if (!emoji.guild.available) return;
+
+  sendLog(client, emoji);
+}
