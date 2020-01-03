c100d62f0ad849a6054711119f0f107bc3e750c4
Jyguy <jskekkx@gmail.com>
2020-01-03 06:56:02 -0500

    remove unneccessary cache checks and add webhook check

--- src/events/message.ts
+++ src/events/message.ts
@@ -7,8 +7,7 @@ const cooldowns = new Set();
 
 export async function run (client: ReknownClient, message: Message) {
   if (message.author!.bot || message.guild && !message.guild.available) return;
-  if (message.guild && message.member!.partial) await message.member!.fetch();
-  if (message.guild && message.guild.me!.partial) await message.guild.me!.fetch();
+  if (message.webhookID) return;
 
   const prefix = message.guild ? await client.functions.getPrefix(client, message.guild.id) : client.config.prefix;
   const regexp = new RegExp(`^<@!?${message.client.user!.id}> `);
