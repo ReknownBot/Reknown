2e609c379a57447074aec73c71080bc947c83248
Jyguy <jskekkx@gmail.com>
2020-01-25 01:38:16 -0500

    improve consistency

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,13 +1,12 @@
-import { GuildMember } from 'discord.js';
 import type { HelpObj } from 'ReknownBot';
 import type ReknownClient from '../../structures/client';
 import type { Message, PermissionString, TextChannel } from 'discord.js';
 
 export async function run (client: ReknownClient, message: Message & { channel: TextChannel }, args: string[]) {
   if (!args[1]) return client.functions.noArg(message, 1, 'a user to kick.');
-  const member = await client.functions.parseMention(args[1], { guild: message.guild!, type: 'member' }).catch(() => false);
+  const member = await client.functions.parseMention(args[1], { guild: message.guild!, type: 'member' }).catch(() => null);
   const reason = args.slice(2).join(' ');
-  if (!(member instanceof GuildMember)) return client.functions.badArg(message, 1, `I did not find a member by that query (${client.escMD(args[0])}).`);
+  if (!member) return client.functions.badArg(message, 1, `I did not find a member by that query (${client.escMD(args[0])}).`);
   if (member.roles.highest.position >= message.member!.roles.highest.position && message.author.id !== message.guild!.ownerID) return client.functions.badArg(message, 1, 'Your role position is not high enough to kick that member.');
   if (!member.kickable) return client.functions.badArg(message, 1, 'I do not have enough powers to kick that member. Please check my permissions and my role position. Note that I cannot kick owners.');
   member.kick(reason);
