b151d7903f458b9a97e9a7acc1692eb6ea9b9ee9
Jyguy <jskekkx@gmail.com>
2020-02-03 16:57:17 -0500

    add warning system

---
+++ src/commands/moderation/warnings.ts
@@ -0,0 +1,53 @@
+import type ReknownClient from '../../structures/client';
+import dateformat from 'dateformat';
+import { tables } from '../../Constants';
+import type { HelpObj, RowWarnings } from 'ReknownBot';
+import type { Message, PermissionString } from 'discord.js';
+import { MessageEmbed, Util } from 'discord.js';
+
+export async function run (client: ReknownClient, message: Message, args: string[]) {
+  const member = args[1] ? await client.functions.parseMention(args[1], {
+    guild: message.guild!,
+    type: 'member'
+  }).catch(() => null) : message.member!;
+  if (!member) return client.functions.badArg(message, 1, 'The member provided does not exist.');
+  if (member.user.bot) return client.functions.badArg(message, 1, 'Bots do not get any warnings.');
+
+  const page = args[2] ? parseInt(args[2]) : 1;
+  if (isNaN(page)) return client.functions.badArg(message, 2, 'The page provided was not a number.');
+  if (page < 1) return client.functions.badArg(message, 2, 'The page provided is lower than one.');
+
+  const { rows }: { rows: RowWarnings[] } = await client.query(`SELECT * FROM ${tables.WARNINGS} WHERE guildid = $1 AND userid = $2 SORT BY warnedat DESC`, [ message.guild!.id, member.id ]);
+  if (rows.length === 0) return message.channel.send(`\`\`${client.escInline(member.user.tag)} (ID: ${member.id})\`\` has \`0\` warnings! Keep up the good work.`);
+
+  const str = (await Promise.all(rows.map(async r => {
+    const user = await client.users.fetch(r.userid);
+    const by = await client.users.fetch(r.warnedby);
+    return `**${dateformat(r.warnedat, 'UTC:mmm d, yyyy, h:MM:ss TT Z')}** \`\`${client.escInline(user.tag)}\`\` by \`\`${client.escInline(by.tag)}\`\` for \`\`${client.escInline(r.warnreason)}\`\``;
+  }))).join('\n');
+  const pages = Util.splitMessage(str, { maxLength: 2048 });
+  if (pages.length > page) return client.functions.badArg(message, 2, `The page provided is out of range. (${pages.length})`);
+
+  const embed = new MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setDescription(pages[page - 1])
+    .setFooter(`Page ${page} of ${pages.length} | Requested by ${message.author.tag}`, message.author.displayAvatarURL())
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: 'jpg', size: 1024 }))
+    .setTitle(`Warnings of ${member.user.tag}`);
+
+  message.channel.send(embed);
+}
+
+export const help: HelpObj = {
+  aliases: [],
+  category: 'Moderation',
+  desc: 'Views past warnings about a member.',
+  togglable: true,
+  usage: 'warnings [Member] [Page=1]'
+};
+
+export const memberPerms: PermissionString[] = [];
+
+export const permissions: PermissionString[] = [
+  'EMBED_LINKS'
+];
