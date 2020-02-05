efe6c6f381108ac63348cb300774901792d3a8bd
Jyguy <jskekkx@gmail.com>
2020-02-05 16:55:04 -0500

    nit(kick): improve consistency

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -6,7 +6,7 @@ import type { Message, PermissionString, TextChannel } from 'discord.js';
 export async function run (client: ReknownClient, message: Message & { channel: TextChannel }, args: string[]) {
   if (!args[1]) return client.functions.noArg(message, 1, 'a user to kick.');
   const member = await client.functions.parseMention(args[1], { guild: message.guild!, type: 'member' }).catch(() => null);
-  const reason = args.slice(2).join(' ');
+  const reason = args[2] ? args.slice(2).join(' ') : undefined;
   if (!member) return client.functions.badArg(message, 1, errors.UNKNOWN_MEMBER);
   if (member.roles.highest.position >= message.member!.roles.highest.position && message.author.id !== message.guild!.ownerID) return client.functions.badArg(message, 1, errors.MEMBER_INSUFFICIENT_POSITION);
   if (!member.kickable) return client.functions.badArg(message, 1, 'I do not have enough powers to kick that member. Please check my permissions and my role position. Note that I cannot kick owners.');
