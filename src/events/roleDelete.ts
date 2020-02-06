d8983b00888709fab66bddd7faebe72f3d204d70
Jyguy <jskekkx@gmail.com>
2020-02-05 21:53:06 -0500

    feat(role logs): add permissions field in creation/deletion

--- src/events/roleDelete.ts
+++ src/events/roleDelete.ts
@@ -1,13 +1,13 @@
-import { MessageEmbed } from 'discord.js';
 import type ReknownClient from '../structures/client';
 import type { Role } from 'discord.js';
+import { MessageEmbed, PermissionString } from 'discord.js';
 
 function sendLog (client: ReknownClient, role: Role) {
-  // const permissions = role.permissions.serialize(false);
+  const permissions = role.permissions.serialize(false);
 
   const embed = new MessageEmbed()
     .addField('Role Name', client.escMD(role.name))
-    // .addField('Permissions', Object.keys(permissions).map(perm => `${perm}: ${permissions[perm as PermissionString]}`).join('\n'))
+    .addField('Permissions', `\`${Object.keys(permissions).map(perm => `${perm}: ${permissions[perm as PermissionString]}`).join('\n')}\``)
     .setColor(client.config.embedColor)
     .setFooter(`ID: ${role.id}`)
     .setTimestamp()
