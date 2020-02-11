109a8784d967f249a381bd712ff955fc170d302b
Jyguy <jskekkx@gmail.com>
2020-02-11 15:16:27 -0500

    fix(reasons): limit reason length in ban and kick

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -7,6 +7,8 @@ export async function run (client: ReknownClient, message: GuildMessage, args: s
   if (!args[1]) return client.functions.noArg(message, 1, 'a user to ban.');
   const member = await client.functions.parseMention(args[1], { guild: message.guild, type: 'member' }).catch(() => null);
   const reason = args[2] ? args.slice(2).join(' ') : undefined;
+  // eslint-disable-next-line no-extra-parens
+  if ((reason?.length ?? 0) > 512) return client.functions.badArg(message, 2, 'The reason length cannot be over 512 characters.');
   if (member) {
     if (member.roles.highest.position >= message.member.roles.highest.position && message.author.id !== message.guild.ownerID) return client.functions.badArg(message, 1, errors.MEMBER_INSUFFICIENT_POSITION);
     if (!member.bannable) return client.functions.badArg(message, 1, 'I do not have enough powers to ban that member. Please check my permissions and my role position. Note that I cannot ban owners.');
