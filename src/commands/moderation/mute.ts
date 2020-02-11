a5311f118b51b63907b3447b1a487a95340389f1
Jyguy <jskekkx@gmail.com>
2020-02-11 16:43:39 -0500

    added ?mute

---
+++ src/commands/moderation/mute.ts
@@ -0,0 +1,89 @@
+import { MessageEmbed } from 'discord.js';
+import type { PermissionString } from 'discord.js';
+import type ReknownClient from '../../structures/client';
+import ms from 'ms';
+import type { GuildMessage, HelpObj, RowMuteRole, RowMutes } from 'ReknownBot';
+import { errors, tables } from '../../Constants';
+
+export async function run (client: ReknownClient, message: GuildMessage, args: string[]) {
+  const row = await client.functions.getRow<RowMuteRole>(client, tables.MUTEROLE, {
+    guildid: message.guild.id
+  });
+  let role = row ? message.guild.roles.cache.get(row.roleid) : message.guild.roles.cache.find(r => r.name === 'Muted');
+  if (!role) {
+    role = await message.guild.roles.create({
+      data: {
+        color: client.config.muteColor,
+        name: 'Muted',
+        permissions: message.guild.roles.everyone!.permissions.remove([ 'ADD_REACTIONS', 'SEND_MESSAGES', 'SPEAK' ])
+      }
+    });
+    client.functions.updateRow<RowMuteRole>(client, tables.MUTEROLE, {
+      guildid: message.guild.id,
+      roleid: role.id
+    }, {
+      guildid: message.guild.id
+    });
+  }
+  if (message.guild.me!.roles.highest.comparePositionTo(role) <= 0) return message.reply(`My highest role has to be higher than the \`\`${client.escInline(role.name)}\`\` role.`);
+
+  if (!args[1]) return client.functions.noArg(message, 1, 'a member to mute.');
+  const member = await client.functions.parseMention(args[1], {
+    guild: message.guild,
+    type: 'member'
+  }).catch(() => null);
+  if (!member) return client.functions.badArg(message, 1, errors.UNKNOWN_MEMBER);
+  if (!member.manageable) return client.functions.badArg(message, 1, 'I do not have enough powers to mute that member. Please check my role position and note that owners cannot be muted.');
+
+  const muteRow = await client.functions.getRow<RowMutes>(client, tables.MUTES, {
+    guildid: message.guild.id,
+    userid: member.id
+  });
+  if (member.roles.cache.has(role.id)) {
+    if (muteRow) await client.query(`DELETE FROM ${tables.MUTES} WHERE guildid = $1 AND userid = $2`, [ message.guild.id, member.id ]);
+    else return client.functions.badArg(message, 1, errors.ALREADY_MUTED);
+  }
+
+  const duration = args[2] ? ms(args[2]) : -1;
+  if (!duration) return client.functions.badArg(message, 2, 'The duration provided was invalid.');
+  if (duration < ms('1m')) return client.functions.badArg(message, 2, 'The duration cannot be shorter than 1 minute.');
+  if (duration > ms('30d')) return client.functions.badArg(message, 2, 'The duration cannot be longer than 30 days.');
+
+  const reason = args[3] ? args.slice(3).join(' ') : undefined;
+  if (reason?.includes('\n')) return client.functions.badArg(message, 2, errors.NO_LINE_BREAKS);
+
+  client.query(`INSERT INTO ${tables.MUTES} (endsat, guildid, userid) VALUES ($1, $2, $3)`, [ Date.now() + duration, message.guild.id, member.id ]);
+  member.roles.add(role);
+  if (duration < ms('20d')) setTimeout(client.functions.unmute, duration, client, member);
+
+  message.channel.send(`Successfully muted \`\`${client.escInline(member.user.tag)}\`\` for \`${client.functions.getFullTime(duration)}\``);
+
+  const embed = new MessageEmbed()
+    .addField('Duration', client.functions.getFullTime(duration))
+    .addField('Reason', reason || 'None')
+    .addField('Muted by', `${message.member} [${client.escMD(message.author.tag)}] (ID: ${message.author.id})`)
+    .setColor(client.config.embedColor)
+    .setFooter(`ID: ${member.id}`)
+    .setThumbnail(member.user.displayAvatarURL({ size: 512 }))
+    .setTimestamp()
+    .setTitle('Member Muted');
+
+  client.functions.sendLog(client, embed, message.guild);
+}
+
+export const help: HelpObj = {
+  aliases: [ 'silence' ],
+  category: 'Moderation',
+  desc: 'Mutes a member. Provide `-1` as the duration for infinite time.',
+  togglable: true,
+  usage: 'mute <Member> [Duration=0] [Reason]'
+};
+
+export const memberPerms: PermissionString[] = [
+  'KICK_MEMBERS',
+  'MANAGE_ROLES'
+];
+
+export const permissions: PermissionString[] = [
+  'MANAGE_ROLES'
+];
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,40 @@
 import { Node } from 'lavalink';
 import type ReknownClient from '../structures/client';
+import type { Snowflake } from 'discord.js';
+import ms from 'ms';
+import { tables } from '../Constants';
+import type { RowMuteRole, RowMutes } from 'ReknownBot';
+
+function invalidateGuild (arr: Snowflake[], client: ReknownClient, guildid: Snowflake) {
+  arr.push(guildid);
+  client.query(`DELETE FROM ${tables.MUTES} WHERE guildid = $1`, [ guildid ]);
+}
+
+async function muteCheck (client: ReknownClient) {
+  const { rows } = await client.query<RowMutes>(`SELECT * FROM ${tables.MUTES}`);
+
+  const invalidGuilds: Snowflake[] = [];
+  rows.forEach(async row => {
+    if (invalidGuilds.includes(row.guildid)) return;
+
+    const guild = client.guilds.cache.get(row.guildid);
+    if (!guild) return invalidateGuild(invalidGuilds, client, row.guildid);
+    if (!guild.me!.hasPermission('MANAGE_ROLES')) return invalidateGuild(invalidGuilds, client, guild.id);
+    const r = await client.functions.getRow<RowMuteRole>(client, tables.MUTEROLE, {
+      guildid: guild.id
+    });
+    const role = r ? guild.roles.cache.get(r.roleid) : guild.roles.cache.find(ro => ro.name === 'Muted');
+    if (!role || guild.me!.roles.highest.comparePositionTo(role) <= 0) return invalidateGuild(invalidGuilds, client, guild.id);
+
+    const member = await guild.members.fetch(row.userid).catch(() => null);
+    if (!member) return client.query(`DELETE FROM ${tables.MUTES} WHERE guildid = $1`, [ row.guildid ]);
+    if (!member.roles.cache.has(role.id)) return client.query(`DELETE FROM ${tables.MUTES} WHERE guildid = $1`, [ row.guildid ]);
+
+    const duration = Number(row.endsat) - Date.now();
+    if (Number(row.endsat) <= Date.now()) member.roles.remove(role);
+    else if (duration < ms('20d')) setTimeout(client.functions.unmute, duration, member);
+  });
+}
 
 export async function run (client: ReknownClient) {
   console.log(`Successfully logged in as ${client.user!.tag} (${client.user!.id}).`);
@@ -16,4 +51,6 @@ export async function run (client: ReknownClient) {
       if (client.guilds.cache.has(guild)) return client.ws.shards.first()!.send(packet);
     },
   });
+
+  muteCheck(client);
 }
