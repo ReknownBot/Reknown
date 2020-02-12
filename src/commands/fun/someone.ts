303fa6c2dba9e55b698ff22fba9713496dcfaff4
Jyguy <jskekkx@gmail.com>
2020-02-12 16:08:11 -0500

    fix(someone): build error

--- src/commands/fun/someone.ts
+++ src/commands/fun/someone.ts
@@ -3,10 +3,9 @@ import type ReknownClient from '../../structures/client';
 import type { GuildMessage, HelpObj } from 'ReknownBot';
 
 export async function run (client: ReknownClient, message: GuildMessage, args: string[]) {
-  let members = message.guild.members;
-  if (members.cache.size !== message.guild.memberCount) members = await message.guild.members.fetch();
+  await message.guild.members.fetch();
 
-  const member = members.cache.filter(m => !m.user.bot).random();
+  const member = message.guild.members.cache.filter(m => !m.user.bot).random();
   message.channel.send(`Successfully randomly fetched **${client.escMD(member.user.tag)}**!`);
 }
 
