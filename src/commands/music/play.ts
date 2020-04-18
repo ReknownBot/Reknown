07565cb9ea91582d171ef4bc92db55a8b6f6bf66
Jyguy <jskekkx@gmail.com>
2020-04-18 02:57:38 -0400

    fix(play): add 3h restriction for songs

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -2,6 +2,7 @@ import type { GuildMessage } from '../../Constants';
 import type { HelpObj } from '../../structures/commandhandler';
 import type { PermissionString } from 'discord.js';
 import type ReknownClient from '../../structures/client';
+import ms from 'ms';
 
 export async function run (client: ReknownClient, message: GuildMessage, args: string[]) {
   let music = client.music[message.guild.id];
@@ -24,6 +25,7 @@ export async function run (client: ReknownClient, message: GuildMessage, args: s
   const song = res.tracks[0];
   if (music.queue.some(s => s.track === song.track)) return client.functions.badArg(message, 1, 'That song is already in the queue.');
   if (music.queue.length >= 15) return client.functions.badArg(message, 1, 'The queue is full! You cannot go over 15 songs at a time.');
+  if (song.info.length > ms('3h')) return client.functions.badArg(message, 1, 'The song cannot be longer than 3 hours.');
 
   if (!music.player) music.player = client.lavalink!.players.get(message.guild.id);
   if (!music.player.playing) await music.player.join(vc.id);
